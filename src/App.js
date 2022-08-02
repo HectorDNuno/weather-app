ad0c506b0dc487e1f2ba1ef2abc99a75870601c7
hector <hectornuno1994@gmail.com>
2022-08-02 15:17:49 -0700

    fills handleSearchChange with two fetch requests to get weather and forecast from api

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import "./App.css";
 import CurrentWeather from "./components/current-weather/CurrentWeather";
 import Search from "./components/search/Search";
+import { WEATHER_API_URL, WEATHER_API_KEY } from "./api";
+import { useState } from "react";
 
 function App() {
+  const [currentWeather, setCurrentWeather] = useState(null);
+  const [forecast, setForecast] = useState(null);
+
   const handleOnSearchChange = (searchData) => {
-    console.log(searchData);
+    const [lat, lon] = searchData.value.split(" ");
+
+    const currentWeatherFetch = fetch(`${WEATHER_API_URL}/weather?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}`);
+    const forecastFetch = fetch(`${WEATHER_API_URL}/forecast?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}`);
+
+    Promise.all([currentWeatherFetch, forecastFetch]).then(async (response) => {
+      const weatherResponse = await response[0].json();
+      const forecastResponse = await response[1].json();
+    });
   };
 
   return (
