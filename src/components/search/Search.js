9d7e0fbfae412c6e181a5ba9e6d732a46b8ad5b2
hector <hectornuno1994@gmail.com>
2022-08-02 14:10:12 -0700

    imports fetch from api, returns object with latitude, longitude, city name, and city country for load options function

--- src/components/search/Search.js
+++ src/components/search/Search.js
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { AsyncPaginate } from "react-select-async-paginate";
+import { GEO_API_URL, geoApiOptions } from "../../api";
 
 const Search = ({ onSearchChange }) => {
   const [search, setSearch] = useState(null);
@@ -9,7 +10,31 @@ const Search = ({ onSearchChange }) => {
     onSearchChange(searchData);
   };
 
-  return <AsyncPaginate placeholder="Search for City" debounceTimeout={600} value={search} onChange={handleOnChange} />;
+  const loadOptions = (inputValue) => {
+    return fetch(`${GEO_API_URL}/cities?minPopulation=1000000&namePrefix=${inputValue}`, geoApiOptions)
+      .then((response) => response.json())
+      .then((response) => {
+        return {
+          options: response.data.map((city) => {
+            return {
+              value: `${city.latitude} ${city.longitude}`,
+              label: `${city.name}, ${city.countryCode}`,
+            };
+          }),
+        };
+      })
+      .catch((err) => console.error(err));
+  };
+
+  return (
+    <AsyncPaginate
+      placeholder="Search for City"
+      debounceTimeout={600}
+      value={search}
+      onChange={handleOnChange}
+      loadOptions={loadOptions}
+    />
+  );
 };
 
 export default Search;
