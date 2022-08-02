2736d9d4f625aa71d6fa24ac26c90ecf7debc144
hector <hectornuno1994@gmail.com>
2022-08-02 15:00:56 -0700

    creates div for bottom half of weather card, creates a temperature section and details section, adds rows to details section

--- src/components/current-weather/CurrentWeather.js
+++ src/components/current-weather/CurrentWeather.js
@@ -10,6 +10,32 @@ const CurrentWeather = () => {
         </div>
         <img alt="weather" className="weather-icon" src="icons/01d.png" />
       </div>
+
+      <div className="bottom">
+        <p className="temperature">100°F</p>
+
+        <div className="details">
+          <div className="parameter-row">
+            <span className="parameter-label">Details</span>
+          </div>
+          <div className="parameter-row">
+            <span className="parameter-label">Feels Like</span>
+            <span className="parameter-value">102°F</span>
+          </div>
+          <div className="parameter-row">
+            <span className="parameter-label">Wind</span>
+            <span className="parameter-value">2 m/s</span>
+          </div>
+          <div className="parameter-row">
+            <span className="parameter-label">Humidity</span>
+            <span className="parameter-value">15%</span>
+          </div>
+          <div className="parameter-row">
+            <span className="parameter-label">Pressure</span>
+            <span className="parameter-value">10 inHg</span>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
