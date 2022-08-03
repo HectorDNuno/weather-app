2240df3df9ecbd16216551a9ee3862e4d1f7ae3d
hector <hectornuno1994@gmail.com>
2022-08-03 14:40:48 -0700

    creates const with days of the week, begins making accordion, creates const that gets the day of the week

---
+++ src/components/forecast/Forecast.js
@@ -0,0 +1,36 @@
+import {
+  Accordion,
+  AccordionItem,
+  AccordionItemButton,
+  AccordionItemHeading,
+  AccordionItemPanel,
+} from "react-accessible-accordion";
+
+const WEEK_DAYS = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+
+const Forecast = ({ data }) => {
+  const dayInAWeek = new Date().getDay();
+
+  return (
+    <>
+      <label className="title">Daily</label>
+      <Accordion allowZeroExpanded>
+        {data.list.splice(0, 7).map((item, index) => (
+          <AccordionItem key={index}>
+            <AccordionItemHeading>
+              <AccordionItemButton>
+                <div className="daily-item">
+                  <img alt="weather" className="icon-small" src={`icons/${item.weather[0].icon}.png`} />
+                  <label className="day"></label>
+                </div>
+              </AccordionItemButton>
+            </AccordionItemHeading>
+            <AccordionItemPanel></AccordionItemPanel>
+          </AccordionItem>
+        ))}
+      </Accordion>
+    </>
+  );
+};
+
+export default Forecast;
