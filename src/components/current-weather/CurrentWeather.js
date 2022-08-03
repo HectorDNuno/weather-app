132880688ad4f9653ab12cac3f42f1efc2108d3f
hector <hectornuno1994@gmail.com>
2022-08-03 14:14:56 -0700

    fixes unit used for wind speed

--- src/components/current-weather/CurrentWeather.js
+++ src/components/current-weather/CurrentWeather.js
@@ -24,7 +24,7 @@ const CurrentWeather = ({ data }) => {
           </div>
           <div className="parameter-row">
             <span className="parameter-label">Wind</span>
-            <span className="parameter-value"> {data.wind.speed} m/s</span>
+            <span className="parameter-value"> {data.wind.speed} mi/hr</span>
           </div>
           <div className="parameter-row">
             <span className="parameter-label">Humidity</span>
